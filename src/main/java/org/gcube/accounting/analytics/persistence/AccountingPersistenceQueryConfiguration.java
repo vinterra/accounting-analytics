8d3dbe9f85995abdcb19378cf0c18b75873c422c
luca.frosini <luca.frosini@mycompany.com>
2015-07-08 14:42:43 +0000

    refs #166: Create accouting-analytics library https://support.d4science.org/issues/166 Implementing Library

---
+++ src/main/java/org/gcube/accounting/analytics/persistence/AccountingPersistenceQueryConfiguration.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package org.gcube.accounting.analytics.persistence;
+
+import org.gcube.accounting.persistence.AccountingPersistenceConfiguration;
+
+/**
+ * @author Luca Frosini (ISTI - CNR) http://www.lucafrosini.com/
+ *
+ */
+public class AccountingPersistenceQueryConfiguration extends AccountingPersistenceConfiguration {
+	
+	protected final String SERVICE_ENDPOINT_NAME = "PersistenceQuery";
+	
+	
+}
