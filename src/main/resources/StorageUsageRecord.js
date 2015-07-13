80e2fe8425985cb7e4f4ae405e01c703c9f310b0
luca.frosini <luca.frosini@mycompany.com>
2015-07-13 13:49:16 +0000

    Adding MapReduce

--- src/main/resources/StorageUsageRecord.js
+++ src/main/resources/StorageUsageRecord.js
@@ -3,8 +3,8 @@
    "language": "javascript",
    "views": {
        "dataVolume": {
-           "map": "function(doc) {\n\tif(doc.usageRecordType == \"StorageUsageRecord\"){\n\t\temit(myKey(doc.creationTime), doc.dataVolume);\n\t}\n}\n\n\nfunction myKey(timestamp){\n\tvar date = new Date(timestamp);\n\tvar key = [];\n\tkey.push(date.getFullYear());\n\tkey.push(date.getUTCMonth());\n\tkey.push(date.getUTCDay());\n\tkey.push(date.getUTCHours());\n\tkey.push(date.getUTCMinutes());\n\tkey.push(date.getUTCSeconds());\n\tkey.push(date.getUTCMilliseconds());\n\treturn key;\n}\n\n",
-           "reduce": "function(keys, values){\n\treturn sum(values);\n}"
+           "map": "function(doc) {\n\tif(doc.usageRecordType == \"StorageUsageRecord\"){\n\t\tvar data = {\n\t\t\t\"dataVolume\" : doc.dataVolume ? doc.dataVolume : 0,\n\t\t\t\"operationCount\" : doc.operationCount ? doc.operationCount :  1,\n\t\t\t\n\t\t};\n\t\tvar dataKey = getDataKey(doc.creationTime);\n\t\temit(dataKey, data);\n\t}\n}\n\n\nfunction getDataKey(timestamp){\n\tvar date = new Date(timestamp);\n\tvar key = [];\n\tkey.push(date.getFullYear());\n\tkey.push(date.getUTCMonth()+1);\n\tkey.push(date.getUTCDate());\n\tkey.push(date.getUTCHours());\n\tkey.push(date.getUTCMinutes());\n\tkey.push(date.getUTCSeconds());\n\tkey.push(date.getUTCMilliseconds());\n\treturn key;\n}\n\n",
+           "reduce": "function(keys, values, rereduce){\n\tvar total = 0;\n\tvar dataVolume = 0;\n\tfor(i=0; i<values.length; i++){\n\t\ttotal += values[i].operationCount;\n\t\tdataVolume += values[i].dataVolume;\n\t}\n\t\n\treturn {\n\t\t\"operationCount\" : total,\n\t\t\"dataVolume\" : dataVolume\n\t};\n}"
        }
    }
 }
