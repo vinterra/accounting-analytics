0515dd173bba168fcfa0bc9da397662c70b7bcb6
luca.frosini <luca.frosini@mycompany.com>
2015-09-07 09:13:20 +0000

    refs #543: Accounting Manager - When the filters are enabled, the serie generated does not match to the time interval required https://support.d4science.org/issues/543

--- src/main/java/org/gcube/accounting/analytics/persistence/AccountingPersistenceQuery.java
+++ src/main/java/org/gcube/accounting/analytics/persistence/AccountingPersistenceQuery.java
@@ -11,12 +11,16 @@ import org.gcube.accounting.analytics.Filter;
 import org.gcube.accounting.analytics.Info;
 import org.gcube.accounting.analytics.TemporalConstraint;
 import org.gcube.accounting.datamodel.AggregatedUsageRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author Luca Frosini (ISTI - CNR) http://www.lucafrosini.com/
  *
  */
 public abstract class AccountingPersistenceQuery {
+	
+	private static final Logger logger = LoggerFactory.getLogger(AccountingPersistenceQuery.class);
 	
 	protected abstract void prepareConnection(AccountingPersistenceQueryConfiguration configuration) throws Exception;
 	
@@ -25,6 +29,9 @@ public abstract class AccountingPersistenceQuery {
 	
 	public Map<Calendar, Info> query(@SuppressWarnings("rawtypes") Class<? extends AggregatedUsageRecord> usageRecordType, 
 			TemporalConstraint temporalConstraint, List<Filter> filters) throws Exception{
+		logger.trace("Request query: UsageRecordType={}, {}={}, {}s={}", usageRecordType.newInstance().getUsageRecordType(), 
+				TemporalConstraint.class.getSimpleName(), temporalConstraint.toString(), 
+				Filter.class.getSimpleName(), filters);
 		return reallyQuery(usageRecordType, temporalConstraint, filters);
 	}
 	
