cf12aab98fa76df31cc64e11d06536e0128d480f
luca.frosini <luca.frosini@mycompany.com>
2016-03-16 12:29:03 +0000

    Reorganizing library

--- src/main/java/org/gcube/accounting/analytics/Filter.java
+++ src/main/java/org/gcube/accounting/analytics/Filter.java
@@ -7,7 +7,7 @@ package org.gcube.accounting.analytics;
  * @author Luca Frosini (ISTI - CNR) http://www.lucafrosini.com/
  *
  */
-public class Filter {
+public class Filter implements Comparable<Filter> {
 
 	protected String key;
 	protected String value;
@@ -47,6 +47,55 @@ public class Filter {
 	 */
 	public void setValue(String value) {
 		this.value = value;
+	}
+	
+	@Override
+	public String toString(){
+		return String.format("{ \"%s\" : \"%s\" }", key, value);
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public int compareTo(Filter filter) {
+		int compareResult = this.key.compareTo(filter.key);
+		if(compareResult == 0){
+			compareResult = this.value.compareTo(filter.value);
+		}
+		return compareResult;
+	}
+	
+	
+	/** {@inheritDoc} */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((key == null) ? 0 : key.hashCode());
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Filter other = (Filter) obj;
+		if (key == null) {
+			if (other.key != null)
+				return false;
+		} else if (!key.equals(other.key))
+			return false;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		return true;
 	}
 	
 }
--- src/main/java/org/gcube/accounting/analytics/persistence/AccountingPersistenceBackendQuery.java
+++ src/main/java/org/gcube/accounting/analytics/persistence/AccountingPersistenceBackendQuery.java
@@ -6,88 +6,98 @@ package org.gcube.accounting.analytics.persistence;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
+import java.util.SortedMap;
+import java.util.SortedSet;
 
 import org.gcube.accounting.analytics.Filter;
 import org.gcube.accounting.analytics.Info;
 import org.gcube.accounting.analytics.TemporalConstraint;
 import org.gcube.documentstore.records.AggregatedRecord;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * @author Luca Frosini (ISTI - CNR) http://www.lucafrosini.com/
- *
  */
 public abstract class AccountingPersistenceBackendQuery {
-	
-	private static final Logger logger = LoggerFactory.getLogger(AccountingPersistenceBackendQuery.class);
 	
 	public static final int KEY_VALUES_LIMIT = 25;
 	
 	protected abstract void prepareConnection(AccountingPersistenceBackendQueryConfiguration configuration) throws Exception;
 	
-	protected abstract Map<Calendar, Info> reallyQuery(@SuppressWarnings("rawtypes") Class<? extends AggregatedRecord> usageRecordType, 
-			TemporalConstraint temporalConstraint, List<Filter> filters) throws Exception;
-	
 	/**
 	 * Query the persistence obtaining a Map where the date is the key and 
 	 * the #Info is the value. The result is relative to an Usage Record Type,
 	 * respect a TemporalConstraint and can be applied one or more filters.
-	 * @param recordClass the Usage Record Type of interest
+	 * @param aggregatedRecordClass the Record Class of interest
 	 * @param temporalConstraint the TemporalConstraint (interval and aggregation)
-	 * @param filters the filter for the query. If null or empty string get all
-	 * data. The filters are evaluated in the order the are presented and are
-	 * considered in AND 
+	 * @param filters list of filter to obtain the time series. If null or 
+	 * empty list get all data for the interested Record Class with the applying 
+	 * temporal constraint. All Filter must have not null and not empty key and 
+	 * value.
+	 * The filters are must be related to different keys and are in AND. 
+	 * If the list contains more than one filter with the same key an Exception
+	 * is thrown. 
 	 * @return the Map containing for each date in the required interval the
 	 * requested data
 	 * @throws Exception if fails
 	 */
-	public Map<Calendar, Info> query(@SuppressWarnings("rawtypes") Class<? extends AggregatedRecord> recordClass, 
-			TemporalConstraint temporalConstraint, List<Filter> filters) throws Exception{
-		logger.trace("Request query: RecordClass={}, {}={}, {}s={}", recordClass.newInstance().getRecordType(), 
-				TemporalConstraint.class.getSimpleName(), temporalConstraint.toString(), 
-				Filter.class.getSimpleName(), filters);
-		return reallyQuery(recordClass, temporalConstraint, filters);
-	}
-	
-	/**
-	 * Return the list of key valid for queries a certain usage record type
-	 * @param recordClass the usage record class 
-	 * @return a set containing the list of key
-	 * @throws Exception if fails
-	 */
-	public abstract Set<String> getKeys(@SuppressWarnings("rawtypes") Class<? extends AggregatedRecord> recordClass) throws Exception;
+	public abstract SortedMap<Calendar, Info> getTimeSeries(
+			Class<? extends AggregatedRecord<?, ?>> aggregatedRecordClass, 
+			TemporalConstraint temporalConstraint, 
+			List<Filter> filters) throws Exception;
 	
 	
 	/**
-	 * Return the list of possible values for a key for a certain usage record 
-	 * type.
-	 * The result are limited to {@link #KEY_VALUES_LIMIT} value. 
-	 * If you want a different limit please use the 
-	 * {@link #getPossibleValuesForKey(Class, String, int)} function.
-	 * Invoking this function has the same effect of invoking 
-	 * {@link #getPossibleValuesForKey(Class, String, int)} function passing 
-	 * {@link #KEY_VALUES_LIMIT} has third argument.
-	 * @param recordClass the usage record type 
-	 * @param key the key 
+	 * Return a SortedMap containing the TimeSeries for top values for a 
+	 * certain key taking in account all Filters. The key is identified
+	 * adding a Filter with a null value. Only one Filter with null value is 
+	 * allowed otherwise an Exception is thrown.
+	 * The values are ordered from the most occurred value.
+	 * @param aggregatedRecordClass the Usage Record Class of interest
+	 * @param temporalConstraint the TemporalConstraint (interval and aggregation)
+	 * @param filters list of filter to obtain the time series. If null or 
+	 * empty list get all data for the interested Record Class with the applying 
+	 * temporal constraint. All Filter (except one) must have not null and not 
+	 * empty key and value. One Filter must have  not null and not 
+	 * empty key and a null value.
+	 * The filters are must be related to different keys and are in AND. 
+	 * If the list contains more than one filter with the same key an Exception
+	 * is thrown. 
+	 * If the list contains more than one filter with null value an Exception
+	 * is thrown.
 	 * @return a set containing the list of possible values
 	 * @throws Exception if fails
 	 */
-	public abstract Set<String> getPossibleValuesForKey(@SuppressWarnings("rawtypes") Class<? extends AggregatedRecord> recordClass, String key) throws Exception;
+	public abstract SortedMap<Filter, Map<Calendar, Info>> getTopValues(
+			Class<? extends AggregatedRecord<?, ?>> recordClass,
+			TemporalConstraint temporalConstraint, List<Filter> filters)
+			throws Exception;
+	
 	
 	/**
-	 * Return the list of possible values for a key for a certain usage record 
-	 * type.
-	 * The result are limited to limit value. When limit is <= 0 this means
-	 * no limit.
-	 * @param recordClass the usage record type 
-	 * @param key the key
-	 * @param limit limit of result to return.
+	 * Return the list of possible values for a key for a certain usageRecord 
+	 * taking in account all Filters. The value for a certain key is identified
+	 * adding a Filter with a null value. Only one Filter with null value is 
+	 * allowed otherwise an Exception is thrown.
+	 * The values are ordered from the most occurred value.
+	 * @param aggregatedRecordClass the Usage Record Class of interest
+	 * @param temporalConstraint the TemporalConstraint (interval and aggregation)
+	 * @param filters list of filter to obtain the time series. If null or 
+	 * empty list get all data for the interested Record Class with the applying 
+	 * temporal constraint. All Filter (except one) must have not null and not 
+	 * empty key and value. One Filter must have  not null and not 
+	 * empty key and a null value.
+	 * The filters are must be related to different keys and are in AND. 
+	 * If the list contains more than one filter with the same key an Exception
+	 * is thrown. 
+	 * If the list contains more than one filter with null value an Exception
+	 * is thrown.
 	 * @return a set containing the list of possible values
 	 * @throws Exception if fails
 	 */
-	public abstract Set<String> getPossibleValuesForKey(@SuppressWarnings("rawtypes") Class<? extends AggregatedRecord> recordClass, String key, int limit) throws Exception;
+	public abstract SortedSet<Filter> getNextPossibleValues(
+			Class<? extends AggregatedRecord<?, ?>> aggregatedRecordClass, 
+			TemporalConstraint temporalConstraint, 
+			List<Filter> filters) throws Exception;
 	
 	/**
 	 * Close the connection to persistence
